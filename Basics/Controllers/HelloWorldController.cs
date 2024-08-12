f89630f2de7e74c9b20a9ca32d5b21eef3d0a158
Ece Irem siser <eceiremsiser@gmail.com>
2024-08-12 15:18:29 +0300

    add welcome action method
    this is my first mvc project. i started with controllers. i learned about url routing logic used by MVC.

---
+++ Basics/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Basics.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        public string Welcome(string name, int x, int y)
+        {
+            return $"Hello {name}, x+y={x+y}";
+        }
+    }
+}
