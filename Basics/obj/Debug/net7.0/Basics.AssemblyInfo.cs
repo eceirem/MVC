6c7e848fd7eec712292b20c3a094de35e12167ae
Ece Irem siser <eceiremsiser@gmail.com>
2024-08-23 16:38:21 +0300

    fix decimal input format- add the css file to wwwroot- add @section{}
    Repository includes site.css for global styles but i need for specific customizations so i created style.css
    i find the problem for decimal format and change Create.cshtml to take right input

--- Basics/obj/Debug/net7.0/Basics.AssemblyInfo.cs
+++ Basics/obj/Debug/net7.0/Basics.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Basics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+712f104e51e2833dc75fd40a85858f8f3e6bc0a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc057c019de1451352e9706cdbf4bec2dd1d7a95")]
 [assembly: System.Reflection.AssemblyProductAttribute("Basics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Basics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
