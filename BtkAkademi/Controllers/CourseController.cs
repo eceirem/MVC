d25ec9d8cf2be974a022ec13f8b7906083a9963a
Ece Irem siser <eceiremsiser@gmail.com>
2024-08-29 18:06:32 +0300

    course application, form handling, model binding, data validation
    This project is a simple example of how to handle forms and check if the data is correct in an ASP.NET Core MVC application. It shows how to collect information from users through a form, make sure the data is valid, and process it using the Candidates model. The project includes pages for both the home and application forms, making it easy to understand the basics of working with forms in ASP.NET Core.

---
+++ BtkAkademi/Controllers/CourseController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BtkAkademi.Models;
+namespace BtkAkademi.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Apply()
+        {
+            return View();
+        }
+         
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        //verinin nereden geldiğini belirtmekte fayda varmış
+        public IActionResult Apply([FromForm] Candidates model)
+        {
+            return View();
+        }
+    }
+}
+    
---
+++ BtkAkademi/Models/Candidates.cs
@@ -0,0 +1,23 @@
+﻿namespace BtkAkademi.Models
+{
+    public class Candidates
+    {
+        public String? Email { get; set; } = String.Empty;
+        public String? FirstName { get; set; } = String.Empty ;
+        public String? LastName { get; set;} = String.Empty;
+
+        public String? Fullname => $"{FirstName} {LastName?.ToUpper()}";
+        public int? Age { get; set; }
+
+        public String? SelectedCourse { get; set; } = String.Empty;
+        public DateTime ApplyAt { get; set; }
+
+        public Candidates() 
+        {
+            ApplyAt = DateTime.Now;
+        }
+
+        
+    }
+
+}
