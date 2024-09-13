ea34636af1bb6363c2fa4ea43d445a66f6a097e5
Ece Irem siser <eceiremsiser@gmail.com>
2024-09-13 12:13:03 +0300

    add a navbar-footer-libman-css-js, worked on Layout
    added links and scripts to layout
    added partial _Navbar to header, partial _Footer to footer
    added icons to Get.cshtml and Index.cstml in fontawesome
    added card and breakpoints to _ProductCard
    worked on sticky footer

--- Store/StoreApp/obj/Debug/net8.0/StoreApp.AssemblyInfo.cs
+++ Store/StoreApp/obj/Debug/net8.0/StoreApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70b203f0cf423e2d96cfa319aed4a3aa8da7184a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7890bd23c400fce1b98300209a77507d1398c9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
