f9b4941c53124b59c5e60a61ddd12f0966b199ae
Ece Irem siser <eceiremsiser@gmail.com>
2024-09-21 15:30:36 +0300

    added viewcomponent

---
+++ Store/StoreApp/Components/ProductSummaryViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Repositories;
+using Services.Contracts;
+
+namespace StoreApp.Components
+{
+	public class ProductSummaryViewComponent : ViewComponent
+	{
+        //Repository dirrekt kullanmak mantıklı değil 
+        //çünkü veritabanında olan her içerik satışta olmayabilir
+        //private readonly RepositoryContext _context;
+        private readonly IServiceManager _manager;
+
+        public ProductSummaryViewComponent(IServiceManager manager)
+		{
+			_manager = manager;
+		}
+
+		public string Invoke()
+		{
+			return _manager.ProductService.GetAllProducts(false).Count().ToString();
+			//return _context.Products.Count().ToString();
+		}
+	}
+}
