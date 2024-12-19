5dcaf4662cacb54fcca36f2d2a8c43907abebd9f
eceirem <eceiremsiser@gmail.com>
2024-12-19 17:50:58 +0300

    authentication

---
+++ Store/StoreApp/Infrastructure/Extensions/ApplicationExtension.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Repositories;
+
+namespace StoreApp.Infrastructure.Extensions
+{
+    public static class ApplicationExtension
+    {
+        public static async void ConfigureDefaultAdminUser(this IApplicationBuilder app)
+        {
+            const string adminUser = "Admin";
+            const string adminPassword = "Admin+123456.";
+            //User manager
+            UserManager<IdentityUser> userManager = app
+                .ApplicationServices
+                .CreateScope()
+                .ServiceProvider
+                .GetRequiredService<UserManager<IdentityUser>>();
+            //Role manager
+            RoleManager<IdentityRole> roleManager = app
+                .ApplicationServices
+                .CreateAsyncScope()
+                .ServiceProvider
+                .GetRequiredService<RoleManager<IdentityRole>>();
+
+            IdentityUser user = await userManager.FindByNameAsync(adminUser);
+
+            if (user is null)
+            {
+                user = new IdentityUser()
+                {
+                    Email = "eceiremsiser@gmail.com",
+                    PhoneNumber = "5078249751",
+                    UserName = adminUser,
+                    EmailConfirmed = true,
+                    PhoneNumberConfirmed = true,
+                    
+                };
+                var result = await userManager.CreateAsync(user, adminPassword);
+                if (!result.Succeeded)
+                {
+                    throw new Exception("Admin user could not created");
+                }
+                var roleResult = await userManager.AddToRolesAsync(user, roleManager.Roles.Select(r => r.Name).ToList());
+
+                if(!roleResult.Succeeded)
+                {
+                    throw new Exception("Admin user could not added to role");
+                }
+            }
+        }
+    }
+}
---
+++ Store/StoreApp/Infrastructure/ServiceExtension.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repositories;
+using Repositories.Contracts;
+using Services.Contracts;
+using Services;
+using Entities.Models;
+using StoreApp.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace StoreApp.Infrastructure
+{
+    public static class ServiceExtension
+    {
+        public static void ConfigureDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<RepositoryContext>(options =>
+            {
+                options.UseSqlite(configuration.GetConnectionString("sqlconnection"),
+                    b => b.MigrationsAssembly("StoreApp"));
+                options.EnableSensitiveDataLogging(true);
+            });
+        }
+
+        public static void ConfigureIdentity(this IServiceCollection services)
+        {
+            services.AddIdentity<IdentityUser, IdentityRole>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = true;
+                options.User.RequireUniqueEmail = true;
+                options.User.AllowedUserNameCharacters =
+                    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+ "; // Customize as needed
+                options.Password.RequireNonAlphanumeric = false; // Disable this rule
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 6;
+            }).AddEntityFrameworkStores<RepositoryContext>();
+
+        }
+        public static void ConfigureSession(this IServiceCollection services)
+        {
+            services.AddDistributedMemoryCache(); //ön bellek sağlıyor
+            services.AddSession(options =>
+            {
+                options.Cookie.Name = "StoreApp.Session";
+                options.IdleTimeout = TimeSpan.FromMinutes(10);
+            });
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            //herkes aynı sepeti kullanmasın
+            services.AddScoped<Cart>(c => SessionCart.GetCart(c));
+        }
+
+        public static void ConfigureRepositoryRegistration(this IServiceCollection services)
+        {
+            services.AddScoped<IRepositoryManager, RepositoryManager>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
+            services.AddScoped<IOrderService, OrderManager>();
+        }
+
+        public static void ConfigureServiceRegistration(this IServiceCollection services)
+        {
+            services.AddScoped<IServiceManager, ServiceManager>();
+            services.AddScoped<IProductService, ProductManager>();
+            services.AddScoped<ICategoryService, CategoryManager>();
+            services.AddScoped<IAuthService, AuthManager>();
+        }
+
+
+    }
+
+}
